6039d93fe38a9f8024df47fb37ea5cc7d8c0e887
bisicus <32414732+bisicus@users.noreply.github.com>
2018-06-04 12:08:59 +0200

    bugfix su ISR();

--- ISR_LED_Fade/ISR_LED_Fade.ino
+++ ISR_LED_Fade/ISR_LED_Fade.ino
@@ -7,107 +7,153 @@
 #define BLUEPIN 9
 
 #define TIMESTEP 50
-#define N 6
+#define EXPRESSIONS 3
+#define NUMCOLOR 16
 #define RGB 3
 #define THRESH 4
 
 /* ---- COLOR VARIABLES ----*/
-byte currentColor[RGB] = {0,0,0};
-byte colors[N][RGB] = {
-  {0, 255, 0},
-  {0, 255, 255},
-  {0, 255, 120},
-  {0, 190, 255},
-  {130, 255, 0},
-  {0, 255, 255}
+byte currentColor[RGB] = {255,0,0};
+byte crntExprIdx;
+
+byte colors[NUMCOLOR][RGB] = {
+  {0, 255, 0},//JOY index=0
+  {0, 255, 255},//1
+  {0, 255, 120},//2
+  {0, 190, 255},//3
+  {130, 255, 0},//4
+  {0, 255, 255},//5
+  {0, 255, 120},//6
+  {0, 255, 255},//7
+  {130, 255, 0},//8
+  {80,0,100},//FEAR index=9
+  {170,0,255},//10
+  {100,0,150},//11
+  {170,0,255},//12
+  {255,0,0},//ANGER index=13
+  {80,40,0},//14
+  {150,20,20}//15
 };
-byte difference[N][RGB];
-byte startColorIndex, endColorIndex;
-int fadeTime[N] = {1000,1000,1000,1000,1000,1000};
+int fadeTime[NUMCOLOR] = {
+  //JOY index=0
+  1000,1000,1000,1000,1000,1000,1000,1000,1000,
+  //FEAR index=9
+  1000,1000,1000,1000,
+  //ANGER index=13
+  1000,1000,1000
+};
+byte exprIdxStart[EXPRESSIONS]={0, 9, 13};
+byte exprIdxEnd[EXPRESSIONS]={8, 12, 15};
+byte difference[NUMCOLOR][RGB];
+byte strtTransitionIdx, endTransitionIdx;
+
 
 /* ---- CONTROL VARIABLES ----*/
-byte i, j, dif, flag;
-unsigned long int nexttime = 0;
+byte i, j, k, dif, flag;
+unsigned long int nexttime = 50;
 
-
-void calcDifference(){
-  for(i=0; i<N-1; i++){
-    for(j=0; j<RGB; j++){
-      difference[i][j] = round((abs((int)colors[i][j] - (int)colors[i+1][j]) * (int)TIMESTEP) / fadeTime[i]);
-      Serial.print(difference[i][j]);
-      Serial.print(',');
-    }
-    Serial.println(' ');
-  }
-  
-  for(j=0; j<RGB; j++){
-    difference[i][j] = round((abs((int)colors[i][j] - (int)colors[0][j]) * TIMESTEP) / fadeTime[i]);
-    Serial.print(difference[i][j]);
-  }
-  Serial.println(' ');
-  Serial.println("---\n");
-}
 
 ISR(TIMER2_OVF_vect){
   if (millis() > nexttime) {
-    Serial.print(currentColor[0]);
-    Serial.print(", ");
-    Serial.print(currentColor[1]);
-    Serial.print(", ");
-    Serial.println(currentColor[2]);
+    cli();
+//    Serial.print(currentColor[0]);
+//    Serial.print(", ");
+//    Serial.print(currentColor[1]);
+//    Serial.print(", ");
+//    Serial.println(currentColor[2]);
+//    Serial.flush();
     //write del colore dato in precedenza contenuto in un array
     analogWrite(REDPIN, currentColor[0]);
     analogWrite(GREENPIN, currentColor[1]);
     analogWrite(BLUEPIN, currentColor[2]);
     
     // Calculate new color for next ISR
-    for(j=0; j<RGB; j++){
-      if(currentColor[j] < colors[endColorIndex][j])
-        currentColor[j] += difference[startColorIndex][j];
-      else
-        currentColor[j] -= difference[startColorIndex][j];
+    for(k = 0; k < RGB; k++){
+      if(currentColor[k] != colors[endTransitionIdx][k]){
+        if(currentColor[k] < colors[endTransitionIdx][k])
+          currentColor[k] += difference[strtTransitionIdx][k];
+        else
+          currentColor[k] -= difference[strtTransitionIdx][k];
+      }
     }
 
     //check if this fading transition has finished finished
-    for(j=0, flag = 0; j<RGB; j++){
-      dif = abs(currentColor[j] - colors[endColorIndex][j]);
-      if(dif>0 && dif<THRESH)
+    for(k=0, flag = 0; k<RGB; k++){
+      dif = abs(currentColor[k] - colors[endTransitionIdx][k]);
+      if(dif>0 && dif<=THRESH)
         flag=1;
     }
-    if(flag){  
-      startColorIndex++;
-      for(j=0; j<RGB; j++){
-        currentColor[j] = colors[startColorIndex][j];
-      }
-      endColorIndex++;
+    if(flag){
+      strtTransitionIdx++;
+      if(strtTransitionIdx > exprIdxEnd[crntExprIdx])
+        strtTransitionIdx = exprIdxStart[crntExprIdx];
       
-      if(endColorIndex == N)
-        endColorIndex=0;
-      if(startColorIndex == N)
-        startColorIndex=0;
-
-      Serial.print("change ");
-      Serial.print(startColorIndex);
-      Serial.println(endColorIndex);
+      endTransitionIdx++;
+      if(endTransitionIdx > exprIdxEnd[crntExprIdx])
+        endTransitionIdx = exprIdxStart[crntExprIdx];
+      
+      for(k=0; k<RGB; k++){
+        currentColor[k] = colors[strtTransitionIdx][k];
+      }
+      
+//      Serial.print("change ");
+//      Serial.print(strtTransitionIdx);
+//      Serial.println(endTransitionIdx);
+//      Serial.flush();
     } 
     nexttime = millis() + TIMESTEP;
+    sei();
   }
 }
 
+void calcDifference(){
+  for(i=0; i<EXPRESSIONS; i++){
+    for(j=exprIdxStart[i]; j<exprIdxEnd[i]; j++){
+      for(k=0; k<RGB; k++){
+        difference[j][k] = round((float)(abs((int)colors[j][k] - (int)colors[j+1][k]) * (int)TIMESTEP) / fadeTime[j]);
+//        Serial.print(difference[j][k]);
+//        Serial.print(',');
+      }
+//      Serial.println(' ');
+//      Serial.flush();
+    }
+    for(k=0; k<RGB; k++){
+      difference[j][k] = round((abs((int)colors[j][k] - (int)colors[exprIdxStart[i]][k]) * TIMESTEP) / fadeTime[j]);
+//      Serial.print(difference[j][k]);
+//      Serial.print(',');
+    }
+//    Serial.println(' ');
+//    Serial.flush();
+  }
+  
+//  Serial.println("---\n");
+}
 
 void setup(){
-  // put your setup code here, to run once:
-  TIMSK2 = (1 << TOIE2);
+  Serial.begin(9600);
   pinMode(REDPIN, OUTPUT);
   pinMode(GREENPIN, OUTPUT);
   pinMode(BLUEPIN, OUTPUT);
-  Serial.begin(9600);
-  sei();
   calcDifference();
-  startColorIndex=0;
-  endColorIndex=1;
+  TIMSK2 = (1 << TOIE2);
 }
 
 void loop() {
-  
+  for(crntExprIdx = 0; crntExprIdx < EXPRESSIONS; crntExprIdx++){
+    cli();
+    strtTransitionIdx = exprIdxStart[crntExprIdx];
+    endTransitionIdx = exprIdxStart[crntExprIdx]+1;
+//    Serial.println("----change----\n");
+//    Serial.print("strtTransitionIdx: ");
+//    Serial.println(strtTransitionIdx);
+//    Serial.print("endTransitionIdx: ");
+//    Serial.println(endTransitionIdx);
+//    Serial.println("--------\n");
+//    Serial.flush();
+    for(k=0; k<RGB; k++){
+      currentColor[k] = colors[strtTransitionIdx][k];
+    }
+    sei();
+    delay(16000);
+  }
 }
