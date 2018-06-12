8a8947736def1ac3df183f6f3e50432311ac2958
Leonardo Bisica <leonardo.bisica@gmail.com>
2018-06-12 16:15:22 +0200

    added RTC class

---
+++ WhaleRTC.cpp
@@ -0,0 +1,25 @@
+#include "Arduino.h"
+#include "WhaleRTC"
+
+#include "RTClib.h"
+
+RTC_PCF8523 rtc;
+
+WhaleRTC::WhaleRTC(){}
+
+void WhaleRTC::init(){
+  if (! rtc.begin()) {
+    Serial.println("RTC Fail. I keep going anyway...");
+    Serial.flush();
+  }  
+  if (! rtc.initialized()) {
+    Serial.println("RTC is NOT running!");
+    Serial.flush();
+  }
+}
+
+void WhaleRTC::getTime(byte* h, byte* m){
+    DateTime now = rtc.now();
+    *h = now.hour();
+    *m = now.minute();
+}
